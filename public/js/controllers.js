e724cf8c58ea8f6c4a494364140e511de32439a4
kiastu <xlimitlessskeisx@gmail.com>
2015-02-01 02:36:12 -0500

    ng-repeat is working properly, the guestbook is full of okans.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,29 +5,42 @@
 angular.module('myApp.controllers', []).
     controller('AppCtrl', function ($scope, $http) {
 
-        $http({
-            method: 'GET',
-            url: '/api/name'
-        }).
-            success(function (data, status, headers, config) {
-                $scope.name = data.name;
-            }).
-            error(function (data, status, headers, config) {
-                $scope.name = 'Error!';
-            });
+        //$http({
+        //    method: 'GET',
+        //    url: '/api/name'
+        //}).
+        //    success(function (data, status, headers, config) {
+        //        $scope.name = data.name;
+        //    }).
+        //    error(function (data, status, headers, config) {
+        //        $scope.name = 'Error!';
+        //    });
 
     }).
     controller('GreetController', function ($scope) {
         // write Ctrl here
 
     }).
-    controller('GuestbookController', function ($scope,$http) {
+    controller('GuestbookController', function ($scope, $http) {
         // write Ctrl here
         //retrieve the button click
-        $scope.saveMessage = function(){
+        $scope.saveMessage = function () {
             alert("The button has been clicked!");
         }
 
+        //make a HTTP request to get the JSON data to populate the list.
+        $http({
+            method: 'GET',
+            url: '/api/posts'
+        }).
+            success(function (data, status, headers, config) {
+                $scope.posts = data;
+            }).
+            error(function (data, status, headers, config) {
+                $scope.posts = [{
+                        name: 'Error!'
+                    }];
+            });
     }).
     controller('SettingsController', function ($scope) {
         // write Ctrl here
